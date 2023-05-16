78ac9c2c207c305108d2a5c5218e81caa85b820b
AdmiralDumbledore <alexanderburdinkzn@gmail.com>
2023-05-16 21:20:58 +0300

    first commit

---
+++ src/main/java/com/pet/richik/RichikApplication.java
@@ -0,0 +1,13 @@
+package com.pet.richik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RichikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RichikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pet/richik/RichikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pet.richik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RichikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
