c86dbab79d6e9aca378516fbdd1b64298db105bd
AdmiralDumbledore <alexanderburdinkzn@gmail.com>
2023-05-17 21:19:20 +0300

    add controller + thymeleaf page

--- src/main/java/com/pet/richik/RichikApplication.java
+++ src/main/java/com/pet/richik/RichikApplication.java
@@ -1,13 +1,16 @@
 package com.pet.richik;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class RichikApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RichikApplication.class, args);
+		log.info("\n = \nServer started \n =");
 	}
 
 }
